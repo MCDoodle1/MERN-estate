8967fdd88e089adec55af91dba76f49d537b5ae9
MCDoodle1 <mclarijs@me.com>
2024-05-25 12:25:28 +0200

    Prepare deploy to render

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Use .env variable
 dotenv.config();
@@ -22,6 +23,9 @@ mongoose
 // Run the server
 const app = express();
 
+// Deploy to Render
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
@@ -36,6 +40,13 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+// Static file to deploy to Render
+app.use(express.static(path.join(__dirname, "/client/dist"))); // "dist" in Vite, "build" in React-create-app
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Middleware
 app.use((err, req, res, next) => {
